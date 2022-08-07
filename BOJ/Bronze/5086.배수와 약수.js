c7ca3105bc310d2329dc0c4cee2c492d00733773
seonnn <ssni0105@gmail.com>
2022-08-07 23:07:53 +0900

    5086.배수와 약수 불필요한 연산 제거

--- BOJ/Bronze/5086.배수와 약수.js
+++ BOJ/Bronze/5086.배수와 약수.js
@@ -4,15 +4,15 @@ let arr = input.map((el) => el.split(" "));
 let result = [];
 
 for (let i = 0; i < arr.length; i++) {
-  let q1 = +arr[i][1] / +arr[i][0];
-  let q2 = +arr[i][0] / +arr[i][1];
+  // let q1 = +arr[i][1] / +arr[i][0];
+  // let q2 = +arr[i][0] / +arr[i][1];
   let r1 = +arr[i][1] % +arr[i][0];
   let r2 = +arr[i][0] % +arr[i][1];
 
   if (+arr[i][0] === 0) break;
 
-  if (q2 > 0 && r2 === 0) result.push("multiple");
-  else if (q1 > 0 && r1 === 0) result.push("factor");
+  if (r2 === 0) result.push("multiple");
+  else if (r1 === 0) result.push("factor");
   else result.push("neither");
 }
 
