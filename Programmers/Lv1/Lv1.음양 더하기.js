e0a4c38e0fa1009d420b62bd87027c6aae68ca16
seonnn <ssni0105@gmail.com>
2022-09-25 23:29:05 +0900

    Lv1.음양 더하기

---
+++ Programmers/Lv1/Lv1.음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+  var answer = 0;
+
+  for (let i = 0; i < absolutes.length; i++) {
+    if (signs[i]) answer += absolutes[i];
+    else answer += absolutes[i] * -1;
+  }
+  return answer;
+}
