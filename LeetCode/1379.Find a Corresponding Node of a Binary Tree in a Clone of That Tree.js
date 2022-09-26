8de0f0e0185cbc7c3545f636a16697b6038c5cbb
seonnn <ssni0105@gmail.com>
2022-09-26 23:59:27 +0900

    1379 주석 추가

--- LeetCode/1379.Find a Corresponding Node of a Binary Tree in a Clone of That Tree.js
+++ LeetCode/1379.Find a Corresponding Node of a Binary Tree in a Clone of That Tree.js
@@ -1,10 +1,15 @@
 var getTargetCopy = function (original, cloned, target) {
   function helper(node, targetNode) {
+    // node가 더 이상 없으면 return
     if (!node) return;
 
+    // node가 있을 때
+    // node.val이 target.val과 같으면 node를 return
     if (node.val === target.val) return node;
+    // 그렇지 않으면 재귀함수 실행
     else return helper(node.left, targetNode) || helper(node.right, targetNode);
   }
 
+  // cloned의 node를 return해야 하기 때문에 helper함수에 clonde와 target을 인자로 넣은 결과값을 return
   return helper(cloned, target);
 };
