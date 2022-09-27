fa3166c376205f3f29b49cbe53d0737a7f718faf
seonnn <ssni0105@gmail.com>
2022-09-28 00:01:20 +0900

    100.Same Tree 주석 추가

--- LeetCode/100.Same Tree.js
+++ LeetCode/100.Same Tree.js
@@ -1,6 +1,10 @@
 var isSameTree = function (p, q) {
   function helper(pNode, qNode) {
+    // pNode와 qNode 둘 다 null일 때 true return
     if (!pNode && !qNode) return true;
+
+    // pNode와 qNode 둘 다 null일 때는 이미 위에서 걸러졌기 때문에
+    // 둘 중 하나가 null이거나 pNode와 qNode의 val이 다를 때 false return
     if (!pNode || !qNode || pNode.val !== qNode.val) return false;
 
     return helper(pNode.left, qNode.left) && helper(pNode.right, qNode.right);
