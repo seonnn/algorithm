fb8d32e619eb3e8e6a3b9715656b4fc12944640c
seonnn <ssni0105@gmail.com>
2022-10-22 22:58:58 +0900

    219.Contains Duplicate II 풀이 및 주석 추가

--- LeetCode/219.Contains Duplicate II.js
+++ LeetCode/219.Contains Duplicate II.js
@@ -1,1 +1,11 @@
-// 미해결
+var containsNearbyDuplicate = function (nums, k) {
+  // 인자 nums(배열) 내 i번째 index와 j번째 index 요소를 비교했을 때
+  //   요소의 값이 같고 인자 각 index 간 차이가 k(숫자) 이하이면 true를 반환
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && j - i <= k) return true;
+    }
+  }
+
+  return false;
+};
