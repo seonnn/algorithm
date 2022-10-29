ac20c7f6b3f6921becb7a23c64ae30c89f6e63a8
seonnn <ssni0105@gmail.com>
2022-10-29 23:39:05 +0900

    49.Group Anagrams 시간복잡도 개선(Map 활용)

--- LeetCode/49.Group Anagrams.js
+++ LeetCode/49.Group Anagrams.js
@@ -1,3 +1,4 @@
+// 풀이1
 var groupAnagrams = function (strs) {
   let newStrs = strs.map((el) => el.split("").sort().join(""));
   let newStrsSet = [...new Set(newStrs)];
@@ -13,4 +14,18 @@ var groupAnagrams = function (strs) {
   result.sort((a, b) => a.length - b.length);
 
   return result;
+};
+
+// 풀이2
+var groupAnagrams = function (strs) {
+  let newStrsMap = new Map();
+
+  for (let str of strs) {
+    let key = str.split("").sort().join("");
+
+    if (newStrsMap.has(key)) newStrsMap.set(key, [...newStrsMap.get(key), str]);
+    else newStrsMap.set(key, [str]);
+  }
+
+  return [...newStrsMap.values()];
 };
