ce005297f13cf64b13796a5efe7fc8ba4346878f
seonnn <ssni0105@gmail.com>
2022-10-30 23:32:16 +0900

    645. Set Mismatch

--- LeetCode/645.Set Mismatch.js
+++ LeetCode/645.Set Mismatch.js
@@ -1,1 +1,9 @@
-// 미해결
+var findErrorNums = function (nums) {
+  let sum = (nums.length * (nums.length + 1)) / 2;
+  let numsSum = nums.reduce((acc, cur) => acc + cur);
+
+  let numsSet = new Set(nums);
+  let numsSetSum = [...numsSet].reduce((acc, cur) => acc + cur);
+
+  return [numsSum - numsSetSum, sum - numsSetSum];
+};
