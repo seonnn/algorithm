a493402c0e749c27b281611e20a0474b5ceb04ac
seonnn <ssni0105@gmail.com>
2022-11-02 00:05:14 +0900

    2231.분해합 코드 수정 및 해결 완료

--- BOJ/Bronze/2231.분해합.js
+++ BOJ/Bronze/2231.분해합.js
@@ -3,17 +3,22 @@ let input = require("fs").readFileSync("/dev/stdin").toString().trim();
 function constructor(input) {
   let len = input.length;
   let startNum = +input - len * 9;
+  let answer = 0;
 
   while (+input >= startNum) {
-    let sumNum =
-      String(startNum)
-        .split("")
-        .reduce((acc, cur) => +acc + +cur) + startNum;
-    if (sumNum === +input) return startNum;
+    let sumNum = String(startNum)
+      .split("")
+      .reduce((acc, cur) => +acc + +cur, startNum);
+
+    if (sumNum === +input) {
+      answer += startNum;
+      break;
+    }
+
     startNum++;
   }
 
-  return 0;
+  return answer;
 }
 
 console.log(constructor(input));
