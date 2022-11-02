54c0248f4d126369f098f10366814aae71855646
seonnn <ssni0105@gmail.com>
2022-11-03 00:32:35 +0900

    Lv2.롤케이크 자르기 풀이 추가

--- Programmers/Lv2/Lv2.롤케이크 자르기.js
+++ Programmers/Lv2/Lv2.롤케이크 자르기.js
@@ -15,3 +15,26 @@ function solution(topping) {
 
   return answer;
 }
+
+// 해결
+function solution(topping) {
+  var answer = 0;
+
+  let map = new Map();
+  for (i in topping) {
+    if (map.get(topping[i])) map.set(topping[i], map.get(topping[i]) + 1);
+    else map.set(topping[i], 1);
+  }
+
+  let set = new Set();
+  for (i in topping) {
+    set.add(topping[i]);
+
+    if (map.get(topping[i]) > 1) map.set(topping[i], map.get(topping[i]) - 1);
+    else map.delete(topping[i]);
+
+    if (map.size === set.size) answer++;
+  }
+
+  return answer;
+}
