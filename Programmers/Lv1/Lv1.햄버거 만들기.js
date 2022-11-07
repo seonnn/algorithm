7d5c9896566feeb633ba4d5814fc01a3c809e97e
seonnn <ssni0105@gmail.com>
2022-11-07 17:18:42 +0900

    Lv1.햄버거 만들기

---
+++ Programmers/Lv1/Lv1.햄버거 만들기.js
@@ -0,0 +1,14 @@
+function solution(ingredient) {
+  var answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < ingredient.length; i++) {
+    stack.push(ingredient[i]);
+    if (stack.slice(-4).join("") === "1231") {
+      answer++;
+      stack.splice(-4);
+    }
+  }
+
+  return answer;
+}
