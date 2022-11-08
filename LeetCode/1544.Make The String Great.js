869ec958cb71b4d18b091933eb2ef657b3d55382
seonnn <ssni0105@gmail.com>
2022-11-09 00:15:37 +0900

    1544.Make The String Great stack 풀이 추가

--- LeetCode/1544.Make The String Great.js
+++ LeetCode/1544.Make The String Great.js
@@ -1,1 +1,29 @@
-var makeGood = function (s) {};
+// 재귀
+var makeGood = function (s) {
+  for (i = 0; i < s.length; i++) {
+    if (Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1)) === 32) {
+      s = s.replace(s[i] + s[i + 1], "");
+      return makeGood(s);
+    }
+  }
+
+  return s;
+};
+
+// stack
+var makeGood = function (s) {
+  let stack = [];
+
+  for (i = 0; i < s.length; i++) {
+    if (
+      stack.length &&
+      Math.abs(s.charCodeAt(i) - stack.at(-1).charCodeAt()) === 32
+    ) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+
+  return stack.join("");
+};
