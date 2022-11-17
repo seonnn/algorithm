24a923cae3aac167ac6f8ed9e58252e2eab2ee7c
seonnn <ssni0105@gmail.com>
2022-11-17 23:53:19 +0900

    374.Guess Number Higher or Lower 이진 탐색 풀이 추가

--- LeetCode/374.Guess Number Higher or Lower.js
+++ LeetCode/374.Guess Number Higher or Lower.js
@@ -9,3 +9,19 @@ var guessNumber = function (n) {
 
   return n;
 };
+
+// 이진 탐색
+var guessNumber = function (n) {
+  let start = 1;
+  let end = n;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    let res = guess(middle);
+
+    if (res === 0) return middle;
+
+    if (res === -1) end = middle - 1;
+    else if (res === 1) start = middle + 1;
+  }
+};
