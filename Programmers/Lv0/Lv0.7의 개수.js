a4da45f36e8faf2a5d65444ea709e7d6619aca3e
seonnn <ssni0105@gmail.com>
2022-12-11 23:51:47 +0900

    Lv0.7의 개수 풀이 추가

--- Programmers/Lv0/Lv0.7의 개수.js
+++ Programmers/Lv0/Lv0.7의 개수.js
@@ -1,12 +1,9 @@
 function solution(array) {
   var answer = 0;
+  let str = array.join("");
 
-  for (let i = 0; i < array.length; i++) {
-    let str = String(array[i]).split("");
-
-    for (let j = 0; j < str.length; j++) {
-      if (str[j] === "7") answer++;
-    }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "7") answer++;
   }
 
   return answer;
