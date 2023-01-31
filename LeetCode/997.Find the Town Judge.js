bdab0ccecea65cac5eae3ae927b700c266755d08
seonnn <ssni0105@gmail.com>
2023-01-31 18:13:40 +0900

    997.Find the Town Judge 풀이 추가(시간복잡도 개선)

--- LeetCode/997.Find the Town Judge.js
+++ LeetCode/997.Find the Town Judge.js
@@ -22,3 +22,26 @@ var findJudge = function (n, trust) {
 
   return -1;
 };
+
+// 풀이 추가(시간복잡도 개선)
+var findJudge = function (n, trust) {
+  if (n === 1 && trust.length === 0) return 1;
+
+  let arr = Array.from({ length: n + 1 }, () => []);
+  let set = new Set();
+
+  for (let i = 0; i < trust.length; i++) {
+    let temp = trust[i];
+
+    if (!arr[temp[1]][0]) arr[temp[1]].push(temp[1]);
+
+    arr[temp[1]].push(temp[0]);
+    set.add(temp[0]);
+  }
+
+  let judge = arr.filter((el) => el.length === n);
+
+  if (!judge.length) return -1;
+
+  return set.has(judge[0][0]) ? -1 : judge[0][0];
+};
