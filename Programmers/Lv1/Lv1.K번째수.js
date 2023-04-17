37297fe73bc8d533e1bf1fe09c81619539727dab
seonnn <ssni0105@gmail.com>
2023-04-17 23:46:48 +0900

    Lv1.K번째수

---
+++ Programmers/Lv1/Lv1.K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let [start, end, target] = commands[i];
+
+    answer.push(array.slice(start - 1, end).sort((a, b) => a - b)[target - 1]);
+  }
+
+  return answer;
+}
