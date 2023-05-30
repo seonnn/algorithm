d28f4a7bcb61a7a516256b8aa4cb29169665ad46
seonnn <ssni0105@gmail.com>
2023-05-30 15:44:36 +0900

    Lv2.2 x n 타일링

---
+++ Programmers/Lv2/Lv2.2 x n 타일링.js
@@ -0,0 +1,22 @@
+function solution(n) {
+  let dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+  }
+
+  return dp[n];
+}
+
+// 효율성 실패
+function solution(n) {
+  let dp = [0, 1, 2];
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+  }
+
+  return dp[n];
+}
