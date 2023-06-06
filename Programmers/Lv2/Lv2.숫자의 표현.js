2f4672b2e1d4979b310b32567f4e3e71e63bbb31
seonnn <ssni0105@gmail.com>
2023-06-06 23:57:35 +0900

    Lv2.숫자의 표현 이전 풀이 추가(효율성 실패)

--- Programmers/Lv2/Lv2.숫자의 표현.js
+++ Programmers/Lv2/Lv2.숫자의 표현.js
@@ -26,3 +26,25 @@ function solution(n) {
 
   return answer + 1;
 }
+
+// 효율성 실패
+function solution(n) {
+  let answer = 0;
+
+  if (n === 1) return 1;
+
+  for (let i = 1; i <= Math.ceil(n / 2); i++) {
+    let sum = 0;
+    let temp = i;
+    while (sum < n) {
+      sum += temp;
+      if (sum === n) {
+        answer++;
+        break;
+      }
+      temp += 1;
+    }
+  }
+
+  return answer + 1;
+}
