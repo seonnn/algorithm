0c5704a2540d4b1aa24fc88f548e9c05310c6cbe
seonnn <ssni0105@gmail.com>
2023-06-06 23:45:58 +0900

    Lv2.숫자의 표현

---
+++ Programmers/Lv2/Lv2.숫자의 표현.js
@@ -0,0 +1,28 @@
+function solution(n) {
+  let answer = 0;
+
+  if (n === 1) return 1;
+
+  for (let i = 2; i <= Math.ceil(n / 2); i++) {
+    if (
+      i % 2 === 1 &&
+      n / i === Math.floor(n / i) &&
+      n / i - Math.floor(i / 2) > 0 &&
+      n / i + Math.floor(i / 2) <= Math.ceil(n / 2)
+    ) {
+      answer += 1;
+      continue;
+    }
+
+    if (
+      i % 2 === 0 &&
+      n / i - 0.5 === Math.floor(n / i) &&
+      n / i - i / 2 + 0.5 > 0 &&
+      n / i + i / 2 - 0.5 <= Math.ceil(n / 2)
+    ) {
+      answer += 1;
+    }
+  }
+
+  return answer + 1;
+}
