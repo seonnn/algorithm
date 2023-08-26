827b062b7211d97ae242cfa1592b11f14d07338a
seonnn <ssni0105@gmail.com>
2023-08-26 23:57:37 +0900

    168. Excel Sheet Column Title

---
+++ LeetCode/168. Excel Sheet Column Title.js
@@ -0,0 +1,44 @@
+var convertToTitle = function (columnNumber) {
+  const alphabet = [
+    "",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const arr = [];
+
+  while (true) {
+    if (columnNumber <= 26) {
+      arr.push(columnNumber);
+      break;
+    }
+
+    arr.push(columnNumber % 26 || 26);
+    columnNumber = Math.floor(columnNumber / 26);
+  }
+
+  return arr.reverse().reduce((acc, cur) => acc + alphabet[cur], "");
+};
