2113dba2f908d5e0e13605d6b1af4313120da339
seonnn <ssni0105@gmail.com>
2023-10-28 23:59:37 +0900

    779.K-th Symbol in Grammar(재귀 풀이 추가)

--- LeetCode/5.Longest Palindromic Substring.js
+++ LeetCode/5.Longest Palindromic Substring.js
@@ -38,3 +38,27 @@ var longestPalindrome = function (s) {
 
   return s.slice(start, end + 1);
 };
+
+// 재귀
+var kthGrammar = function (n, k) {
+  if (n === 1) {
+    return 0;
+  }
+
+  if (n === 2) {
+    if (k === 1) {
+      return 0;
+    }
+
+    if (k === 2) {
+      return 1;
+    }
+  }
+
+  const index = k % 2;
+  const prevK = Math.ceil(k / 2);
+
+  const prevBit = kthGrammar(n - 1, prevK);
+
+  return Math.abs(prevBit - index ? 0 : 1);
+};
