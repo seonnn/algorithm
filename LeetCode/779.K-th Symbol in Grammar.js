261b46207894a0ebc07e861d6b3427ce57d5f3a7
seonnn <ssni0105@gmail.com>
2023-10-29 23:23:36 +0900

    779.K-th Symbol in Grammar(재귀 풀이 리팩토링)

--- LeetCode/779.K-th Symbol in Grammar.js
+++ LeetCode/779.K-th Symbol in Grammar.js
@@ -15,3 +15,13 @@ var kthGrammar = function (n, k) {
   if (reversed) return k === 1 ? 1 : 0;
   else return k === 1 ? 0 : 1;
 };
+
+// 재귀
+var kthGrammar = function (n, k) {
+  if (n === 1) return 0;
+
+  const mid = Math.pow(2, n - 1) / 2;
+
+  if (k <= mid) return kthGrammar(n - 1, k);
+  else return 1 - kthGrammar(n - 1, k - mid);
+};
