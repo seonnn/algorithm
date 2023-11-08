4a9e78abec8e0e787d5a3fe7932b52a031ae9bb0
seonnn <ssni0105@gmail.com>
2023-11-08 23:59:26 +0900

    Lv1.기사단원의 무기 리팩토링

--- Programmers/Lv1/Lv1.기사단원의 무기.js
+++ Programmers/Lv1/Lv1.기사단원의 무기.js
@@ -9,9 +9,9 @@ function solution(number, limit, power) {
 }
 
 function divisorCounter(n, limit, power) {
-  let res = 0;
+  let res = 1;
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= n / 2; i++) {
     if (n % i === 0) {
       res += 1;
     }
