c30b54314deeba22d1e3949b9bc6b74e6dc86ea0
seonnn <ssni0105@gmail.com>
2023-11-19 00:29:51 +0900

    1838.Frequency of the Most Frequent Element 주석 추가

--- LeetCode/1838.Frequency of the Most Frequent Element.js
+++ LeetCode/1838.Frequency of the Most Frequent Element.js
@@ -8,6 +8,8 @@ var maxFrequency = function (nums, k) {
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
 
+    /* 앞에 있는 숫자들을 nums에 i와 동일하게 만들기 위해 필요한 수(nums[i] * window의 길이 - window 내 숫자들의 합)가 k보다 크면
+    pointer를 우측으로 이동시켜 window의 길이를 조정함 */
     while (nums[i] * (i - pointer + 1) - sum > k) {
       sum -= nums[pointer];
       pointer++;
