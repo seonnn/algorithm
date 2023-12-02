e38e70366fb19c68ae551393bd8a95bcd589b10d
seonnn <ssni0105@gmail.com>
2023-12-02 23:57:27 +0900

    1662.Check If Two String Arrays are Equivalent 풀이 추가

--- LeetCode/1662.Check If Two String Arrays are Equivalent.js
+++ LeetCode/1662.Check If Two String Arrays are Equivalent.js
@@ -4,3 +4,8 @@ var arrayStringsAreEqual = function (word1, word2) {
 
   return word1 === word2;
 };
+
+// reduce 메서드 사용
+var arrayStringsAreEqual = function (word1, word2) {
+  return word1.reduce((acc, cur) => acc + cur, "") === word2.reduce((acc, cur) => acc + cur, "");
+};
