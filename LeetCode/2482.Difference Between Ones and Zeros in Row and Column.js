e861327f6d86aa2a7e1e284fb9c987e50cc129d6
seonnn <ssni0105@gmail.com>
2023-12-17 23:59:24 +0900

    2482.Difference Between Ones and Zeros in Row and Column

---
+++ LeetCode/2482.Difference Between Ones and Zeros in Row and Column.js
@@ -0,0 +1,23 @@
+var onesMinusZeros = function (grid) {
+  let res = Array.from({ length: grid.length }, () => Array(grid[0].length).fill(0));
+  let row = {};
+  let col = {};
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        if (!col[j]) col[j] = [0, 0];
+        if (!row[i]) row[i] = [0, 0];
+        col[j][1] += 1;
+        row[i][1] += 1;
+      } else {
+        if (!col[j]) col[j] = [0, 0];
+        if (!row[i]) row[i] = [0, 0];
+        col[j][0] += 1;
+        row[i][0] += 1;
+      }
+    }
+  }
+
+  console.log(row, col);
+};
